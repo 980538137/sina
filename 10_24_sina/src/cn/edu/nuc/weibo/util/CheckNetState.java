4d90ff5e0ae36c53dd2923db4f0a86f8e198e7eb
songguoxing <980538137@qq.com>
2013-05-06 20:21:07 +0800

    formatting

--- 10_24_sina/src/cn/edu/nuc/weibo/util/CheckNetState.java
+++ 10_24_sina/src/cn/edu/nuc/weibo/util/CheckNetState.java
@@ -5,16 +5,21 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo.State;
 
 public class CheckNetState {
-	public static boolean checkNetworkState(Context context){
-		ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-		State state_mobile = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
-		State state_wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
-		if (state_mobile == State.CONNECTED || state_mobile == state_mobile.CONNECTING) {
-			return true;
-		}else if (state_wifi == State.CONNECTED || state_wifi == state_mobile.CONNECTING) {
-			return true;
-		}
-		return false;
-	}
+    public static boolean checkNetworkState(Context context) {
+        ConnectivityManager connManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        State state_mobile = connManager.getNetworkInfo(
+                ConnectivityManager.TYPE_MOBILE).getState();
+        State state_wifi = connManager.getNetworkInfo(
+                ConnectivityManager.TYPE_WIFI).getState();
+        if (state_mobile == State.CONNECTED
+                || state_mobile == State.CONNECTING) {
+            return true;
+        } else if (state_wifi == State.CONNECTED
+                || state_wifi == State.CONNECTING) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- 10_24_sina/src/cn/edu/nuc/weibo/util/NetUtil.java
+++ 10_24_sina/src/cn/edu/nuc/weibo/util/NetUtil.java
@@ -6,24 +6,27 @@ import java.net.URL;
 import android.graphics.drawable.Drawable;
 
 public class NetUtil {
-	/**
-	 * 获取网络上的图片
-	 * @param url
-	 * @return
-	 */
-	public static Drawable getNetImage(String url){
-		if (url == null||url == "") {
-			return null;
-		}
-		try {
-			URL url2 = new URL(url);
-			HttpURLConnection connection = (HttpURLConnection)url2.openConnection();
-			return Drawable.createFromStream(connection.getInputStream(), "image");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
-	}
+    /**
+     * 获取网络上的图片
+     * 
+     * @param url
+     * @return
+     */
+    public static Drawable getNetImage(String url) {
+        if (url == null || url == "") {
+            return null;
+        }
+        try {
+            URL url2 = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) url2
+                    .openConnection();
+            return Drawable.createFromStream(connection.getInputStream(),
+                    "image");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
